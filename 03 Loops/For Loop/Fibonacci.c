79a2fa7c262b12c9d99ebbf935c1bdafb588ad38
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-04-16 12:00:49 +0530

    Source Code

--- 03 Loops/For Loop/Fibonacci.c
+++ 03 Loops/For Loop/Fibonacci.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int n, first = 0, second = 1, next;
+    int n;
+    int first = 0, second = 1, next;
 
     printf("Enter the number of terms : ");
     scanf("%d", &n);
 
-    printf("Fibonacci number : ");
-    for (int i = 1; i <= n; ++i) {
+    printf("Fibonacci Series up to %d terms : ", n);
+
+    for (int i = 1; i <= n; i++) {
         printf("%d ", first);
         next = first + second;
         first = second;
