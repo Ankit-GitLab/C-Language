199e2b2fd50d03a526d6b919487bb8b93028c8db
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-04-26 20:56:46 +0530

    Source Code

--- 02 Conditionals/LeapYear.c
+++ 02 Conditionals/LeapYear.c
@@ -4,8 +4,9 @@ int main()
     int N;
     printf("Enter a year : ");
     scanf("%d", &N);
-    if (N % 4 == 0)
+    if ((N % 4 == 0 || N % 100 == 0) && N % 400 == 0){
         printf("leap year ");
+    }
     else
         printf("Not Leap Year");
     return 0;
