d6dfa13a6a9f698f74edb1423902d3fe2ce886ab
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-04-26 23:37:11 +0530

    Source Code

--- 08 Arrays/BasicArray/BasicArrayChar.c
+++ 08 Arrays/BasicArray/BasicArrayChar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    char arr[5] = {'a', 'f', 'e', '%'};
+    char arr[6] = {'a', 'f', 'e', '%','e'};
     printf("%c ", arr[0]);
     printf("%c ", arr[2]);
     printf("%c ", arr[3]);
