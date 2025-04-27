3ef0da4df81da8b939f552a891b09db93885195f
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-04-27 11:34:17 +0530

    Source Code

--- 16 Structure/Type_def.c
+++ 16 Structure/Type_def.c
@@ -1,27 +1,15 @@
-#include <stdio.h>
-
-// Define a structure for Student
-struct Student {
-    int rollNo;         // To store roll number
-    char name[10];      // To store name (max 9 characters + null terminator)
-    float marks;        // To store marks
-};
+#include <stdio.h> // Including standard input-output library
 
 int main() {
-    // Declare and initialize an array of 5 Student structures
-    struct Student Ankit[5] = {
-        {3,  "Ankit",  95.0},     // Student 1
-        {23, "Vedant", 96.45},    // Student 2
-        {45, "Vishal", 45.6},     // Student 3
-        {22, "Ravi",   98.5},     // Student 4
-        {41, "Suraj",  100.0}     // Student 5
-    };
+    typedef int Ankit;    // Creating an alias 'Ankit' for 'int' type
+    typedef float Vedant; // Creating an alias 'Vedant' for 'float' type
 
-    // Loop to print details of each student
-    for (int i = 0; i < 5; i++) {
-        printf("\nStudent Name: %s, Roll No: %d, Marks: %.2f", 
-               Ankit[i].name, Ankit[i].rollNo, Ankit[i].marks);
-    }
+    Ankit num1 = 5;       // Declaring an integer variable 'num1' and assigning 5
+    Vedant num2 = 6.5;    // Declaring a float variable 'num2' and assigning 6.5
 
-    return 0;  // Return 0 to indicate successful execution
+    Vedant sum = num1 + num2;  // Adding num1 and num2, result stored in 'sum' (float)
+
+    printf("Sum is = %f\n", sum); // Printing the sum with %f format specifier for float
+
+    return 0;  // Returning 0 to indicate successful program termination
 }
