ccf6904134d3aed3670ae159e53964cb042124b7
Ankit Kumar <ankit_246003@saitm.ac.in>
2025-06-11 10:52:50 +0530

    Update Some Things

---
+++ File_Handling/Fopen_Fclose.c
@@ -0,0 +1,9 @@
+ #include<stdio.h>
+ int main(){
+    FILE*ptr = NULL;
+    ptr = fopen("myfile.txt","r");
+    char c = fgetc(ptr);
+    printf("The character I read was %c\n", c);
+   return 0;
+
+ } 
