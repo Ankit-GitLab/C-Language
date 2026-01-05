365931276a1cba3efdde1252ddb4658680464f14
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-01-05 15:52:36 +0530

    In @d array

---
+++ Z_Exam/Unit_1/Deletion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int arr[2][2] = {{2,4},{3,4}};
+    int r = 2, c = 2;      // current size
+    int x = 0, y = 1;      // deletion position
+    int val = 99;          // value to insert
+    
+    arr[x][y] = 0;
+       for(int i=0; i<2; i++){
+        for(int j=0; j<2; j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
