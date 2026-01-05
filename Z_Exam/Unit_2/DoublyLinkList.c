2399450e626e4ff4c56481aa71b6803a98ab193e
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-01-05 19:34:01 +0530

    Linklist

---
+++ Z_Exam/Unit_2/DoublyLinkList.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+int main(){
+    struct Node *n1, *n2, *n3;
+    
+
+    return 0;
+}
