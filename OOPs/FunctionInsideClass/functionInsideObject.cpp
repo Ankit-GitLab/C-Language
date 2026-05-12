68731ce7c8478c234d161c82dfe0df7af65413f8
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-05-12 14:53:10 +0530

    function inside object create function

---
+++ OOPs/FunctionInsideClass/functionInsideObject.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Cricketer{
+public:
+    string name;
+    int runs;
+    float avg;
+
+    Cricketer(string name, int runs,float avg){
+        this->name = name;
+        this->runs = runs;
+        this->avg = avg;
+    }
+
+    void print(){
+        cout<<name<<" "<<runs<<" "<<avg<<endl;
+       
+    }
+};
+int main(){
+    Cricketer c1("virat kohli",25000,55.2);
+    Cricketer c2("Rohit Sharma",18000,47.8);
+
+    c1.print();
+    c2.print();
+}
