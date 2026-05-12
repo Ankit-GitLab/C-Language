a8579e0e285366391101d5bf060d2a05a9ad821a
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-05-12 13:43:25 +0530

    this class

---
+++ OOPs/passing object/PassingReference.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+class Car{ // Car is a new data type
+public:
+    string name;
+    int price;
+    int seats;
+    string type;
+     
+};
+
+void print(Car c){
+    cout<<c.name<<" "<<c.price<<" "<<c.seats<<" "<<c.type<<endl;
+}
+
+void change(Car& c){ //pass by reference 
+   c.name = "Audi A8";
+   
+}
+
+int main(){
+
+
+    Car c1;
+    c1.name = "Honda city";
+    c1.price = 1500000;
+    c1.seats = 5;
+    c1.type  = "sedan";
+
+    print(c1);
+    change(c1);
+    print(c1);
+
+}
